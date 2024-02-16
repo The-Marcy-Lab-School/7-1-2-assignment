50dd87cfaa399b9b5fcc5986ce262b2a500623d9
benspector-mls <ben@marcylabschool.org>
2024-02-16 15:42:07 -0800

    init

---
+++ vite-project/src/context/RobotProvider.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+import RobotContext from './RobotContext'
+import { handleFetch } from '../utils'
+
+const RobotProvider = ({ children }) => {
+    const [robots, setRobots] = useState([])
+
+    useEffect(() => {
+        const doFetch = async () => {
+            const [data, error] = await handleFetch('http://localhost:4000/robots');
+            if (error) return;
+            setRobots(data);
+        }
+        doFetch();
+    }, [])
+
+    const value = {
+        robots
+    }
+
+    return (
+        <RobotContext.Provider value={value}>
+            {children}
+        </RobotContext.Provider>
+    )
+}
+
+export default RobotProvider
